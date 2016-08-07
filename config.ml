35af21be6513fc43a77796a953326cf6dbd4e6bd
Richard Mortier <mort@cantab.net>
2016-08-07 15:36:46 +0200

    simple configuration

--- config.ml
+++ config.ml
@@ -18,23 +18,6 @@ let https_port =
   let doc = Key.Arg.info ~doc:"Listening HTTPS port." ["https_port"] in
   Key.(create "https_port" Arg.(opt int 4433 doc))
 
-let certificate =
-  let doc = Key.Arg.info
-      ~docv:"FILE"
-      ~doc:"The full path to PEM encoded certificate chain FILE\
-           \ (may also include the private key)"
-      ["cert"]
-  in
-  Key.(create "cert" Arg.(required string doc))
-
-let privkey =
-  let doc = Key.Arg.info
-      ~docv:"FILE"
-      ~doc:"The full path to PEM encoded unencrypted private key in FILE"
-      ["privkey"]
-  in
-  Key.(create "privkey" Arg.(required string doc))
-
 let haproxy1 =
   let doc = Key.Arg.info
       ~doc:
@@ -44,3 +27,52 @@ let haproxy1 =
       ["haproxy1"]
   in
   Key.(create "haproxy1" Arg.(opt bool false doc))
+
+(* can't create devices from parameters at present; cf
+     https://github.com/mirage/mirage/issues/571#issuecomment-237903266
+
+let certificate =
+  let doc = Key.Arg.info
+      ~docv:"FILE"
+      ~doc:"The full path to PEM encoded certificate chain FILE\
+           \ (may also include the private key)"
+      ["cert"]
+  in
+  Key.(create "cert" Arg.(required ~stage:`Configure string doc))
+
+let privkey =
+  let doc = Key.Arg.info
+      ~docv:"FILE"
+      ~doc:"The full path to PEM encoded unencrypted private key in FILE"
+      ["privkey"]
+  in
+  Key.(create "privkey" Arg.(required ~stage:`Configure string doc))
+
+(** Devices *)
+
+let privkey_disk =
+  match Key.(default @@ value privkey) with
+  | None -> failwith "BAD PRIVKEY! BAD!"
+  | Some file -> generic_kv_ro file
+
+let certificate_disk =
+  match Key.(default @@ value certificate) with
+  | None -> failwith "BAD CERTIFICATE! BAD!"
+  | Some file -> generic_kv_ro file
+*)
+
+let privkey_disk = generic_kv_ro "privkey"
+let certificate_disk = generic_kv_ro "certificate"
+
+(** Go! *)
+let main =
+  let keys =
+    let a = Key.abstract in
+    [ a http_ip; a http_port; a https_ip; a https_port; a haproxy1 ]
+  in
+  foreign ~keys "Tlstunnel.Main" (clock @-> kv_ro @-> kv_ro @-> job)
+
+let () =
+  register "tlstunnel" [
+    main $ default_clock $ certificate_disk $ privkey_disk
+  ]
